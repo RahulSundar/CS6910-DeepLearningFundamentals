5e0dadebf1da85f6a86229e062d8894e150470c3
Rahul Sundar <rahul.sundar95@gmail.com>
2021-02-13 18:41:30 +0530

    Stage0: Some refinements in plots and gray scale

--- Assignment1/dataPreprocessing.py
+++ Assignment1/dataPreprocessing.py
@@ -12,18 +12,19 @@ img_classes_fig = plt.figure("The image classes")
 grid_specification = img_classes_fig.add_gridspec(2,5)
 
 num_classes = int(np.max(trainOut) + 1)
+class_dictionary = {"0":"T-shirt/Top","1":"Trouser","2":"Pullover","3":"Dress","4":"Coat","5":"Sandal","6":"Shirt","7":"Sneaker","8":"Bag","9":"Ankle Boot"}
 ctr = 0
 for i in range(num_classes):
     for j in range(trainIn.shape[0]):
         if trainOut[j] == i and i < 5:
             globals()["ax"+str(i+1)]=img_classes_fig.add_subplot(grid_specification[0,i]) 
-            plt.imshow(trainIn[j])
-            plt.label(str(i))
+            plt.imshow(trainIn[j], cmap = 'gray')
+            plt.xlabel(class_dictionary[str(i)], fontsize = 15)
             break
         elif trainOut[j] == i and i >= 5:
             globals()["ax"+str(5 + i + 1)]=img_classes_fig.add_subplot(grid_specification[1,i-5]) 
-            plt.imshow(trainIn[j])
-            plt.label(str(i))
+            plt.imshow(trainIn[j], cmap = 'gray')
+            plt.xlabel(class_dictionary[str(i)], fontsize = 15)
             break            
 
- 
+plt.show()

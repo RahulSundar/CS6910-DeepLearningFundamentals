9df6907e208b3595641f98fd52743da113c1658e
Rahul Sundar <rahul.sundar95@gmail.com>
2021-03-13 23:10:56 +0530

    Stage3: Removed unnecessary files, cleaned up the codes for activation functions.

--- Assignment1/utility/activation.py
+++ Assignment1/utility/activation.py
@@ -2,36 +2,36 @@ import numpy as np
 
 
 def sigmoid(z):
-    return 1.0/ (1 + np.exp(-(z)))
+    return 1.0 / (1 + np.exp(-(z)))
 
 
 def tanh(z):
     return np.tanh(z)
-    
-    
+
+
 def sin(z):
     return np.sin(z)
-    
-    
-def reLu(z):
-    return np.max(z,0)
-    
+
+
+def relu(z):
+    return np.max(z, 0)
+
 
 def softmax(Z):
-    return np.exp(Z)/np.sum(np.exp(Z))
-    
-    
+    return np.exp(Z) / np.sum(np.exp(Z))
+
+
 def der_sigmoid(z):
-    return sigmoid(z)(1- sigmoid(z))
-    
+    return sigmoid(z)(1 - sigmoid(z))
+
+
 def der_tanh(z):
-    return (1 - np.tanh(z)**2)
+    return 1 - np.tanh(z) ** 2
 
 
-def der_reLU(z):
+def der_relu(z):
     if z < 0:
         a = 0
     else:
         a = 1
     return a
-    

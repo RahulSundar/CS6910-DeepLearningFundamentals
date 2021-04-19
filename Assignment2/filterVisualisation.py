24c37b0e2615f66ff1d1bb457c4d8332a48eb9f0
Rahul Sundar <rahul.sundar95@gmail.com>
2021-04-19 08:01:09 +0530

    Final Commit: Filter and Feature map visualisation on a random test image.

--- Assignment2/filterVisualisation.py
+++ Assignment2/filterVisualisation.py
@@ -1,10 +1,15 @@
 import numpy as np
+from matplotlib import pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+
+
 
 import wandb
 from wandb.keras import WandbCallback
 
+
+#To let the gpu memory utilisiation grow as per requirement
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
@@ -12,13 +17,17 @@ except:
 #Invalid device or cannot modify virtual devices once initialized.
     pass
 
-
-# build the VGG16 network
-source_model = keras.models.load_model("./TrainedModel/Best_trained_Model/Best_trained_Model") #Load the best trained model
-
-# get the symbolic outputs of each "key" layer (we gave them unique names).
-source_model.summary()
-
+def class_names(DATAPATH):
+   
+    class_name=[]
+    
+    for dir1 in np.sort(os.listdir(DATAPATH)):
+        class_name.append(dir1)
+    return class_name
+    
+class_names = class_names("./Data/inaturalist_12K/test/")
+target_dict = {k: v for v, k in enumerate(np.unique(Class_names))} 
+class_label_names_dict = {str(k): v for k, v in enumerate(np.unique(Class_names))} 
 # The dimensions of our input image
 img_width,img_height = 128, 128
 
@@ -27,75 +36,111 @@ test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
 test_generator = test_datagen.flow_from_directory(
             './Data/inaturalist_12K/test',
-            target_size=IMG_SIZE,
-            batch_size=BATCH_SIZE,
+            target_size=(img_width,img_height),
+            batch_size=32,
             class_mode='categorical',
-            shuffle = True,
-            seed = 123)
+            shuffle = False)
 # Our target layer: we will visualize the filters from this layer.
+
+source_model = keras.models.load_model("./TrainedModel/Best_Model") #Load the best trained model
+
 # See `model.summary()` for list of layer names, if you want to change this.
-layer_dict = {layer.name:layer for layer in model.layers}
+source_model.summary()
+layer_dict = {layer.name:layer for layer in source_model.layers}
 print(layer_dict)
 #Task: to  build a simple terminal user interface
-#
+
+
+#1st Convolutional layer:
 layer_name = "conv2d" 
+activation_layer_name = "activation"
 
-model = Sequential()
-for layer in source_model.layers[:-2]: # go through until last layer
-    model.add(layer)
-#model.add(Dense(3, activation='softmax'))
-model.summary()
+layer = source_model.get_layer(name=layer_name)
+activation_layer = source_model.get_layer(name=activation_layer_name)
 
-layer = model.get_layer(name=layer_name)
-feature_extractor = keras.Model(inputs=model.inputs, outputs=layer.output)
+feature_extractor = keras.Model(inputs=source_model.inputs, outputs=layer.output)
+feature_extractor_activation = keras.Model(inputs=source_model.inputs, outputs=activation_layer.output)
 
 
-layer = model.get_layer(name=layer_name)
 filters, biases = layer.get_weights()
+
+
 # normalize filter values to 0-1 so we can visualize them
-f_min, f_max = filters.min(), filters.max()
-filters = (filters - f_min) / (f_max - f_min)
+filter_min, filter_max = filters.min(), filters.max()
+filters = (filters - filter_min) / (filter_max - filter_min)
+
+
 # plot first few filters
-n_filters, ix = 6, 1
+n_filters, ix = 32, 1
 for i in range(n_filters):
 	# get the filter
 	f = filters[:, :, :, i]
 	# plot each channel separately
 	for j in range(3):
 		# specify subplot and turn of axis
-		ax = pyplot.subplot(n_filters, 3, ix)
+		ax = plt.subplot( 3, n_filters, ix)
 		ax.set_xticks([])
 		ax.set_yticks([])
 		# plot filter channel in grayscale
-		pyplot.imshow(f[:, :, j], cmap='gray')
+		plt.imshow(f[:, :, j], cmap='gray')
 		ix += 1
 # show the figure
-pyplot.show()
+plt.show()
 
-
+#To get filters of all conv layers uncomment the following:
+'''
 i = 0 
 for layer in model.layers: 
     if 'conv' not in layer.name: 
         continue 
     i += 1 
     globals()["filters"+ str(i)],globals()["biases"+str(i)] = layer.get_weights()
-    
-    
-model2 = keras.Model(inputs=model.inputs, outputs=layer.output)
-feature_maps = model2.predict(img) 
+'''    
+
+batch = np.random.choice(int(2000/32))
+img_index = np.random.choice(32)    
+img = test_generator[batch][0][img_index]
+img = np.expand_dims(img, axis = 0)
+img_true_label = test_generator[batch][1][img_index]
+
+#plot the test image:
+plt.figure()
+plt.xlabel(class_label_names_dict[str(np.argmax(img_true_label))])
+plt.imshow(img[0])
 
 
-square = 8
+#Extract the feature maps and feature activation maps
+feature_maps = feature_extractor(img) 
+feature_maps_activation = feature_extractor_activation(img) 
+
+#32 filters in the first layer:
+ROWS = 4
+COLUMNS = 8 
 ix = 1
-for _ in range(square):
-	for _ in range(square):
+for _ in range(ROWS):
+	for _ in range(COLUMNS):
 		# specify subplot and turn of axis
-		ax = pyplot.subplot(square, square, ix)
+		ax = plt.subplot(ROWS, COLUMNS, ix)
 		ax.set_xticks([])
 		ax.set_yticks([])
 		# plot filter channel in grayscale
-		pyplot.imshow(feature_maps[0, :, :, ix-1], cmap='gray')
+		plt.imshow(feature_maps[0, :, :, ix-1], cmap='gray')
 		ix += 1
 # show the figure
-pyplot.show() 
+plt.show() 
+
+ROWS = 4
+COLUMNS = 8 
+ix = 1
+for _ in range(ROWS):
+	for _ in range(COLUMNS):
+		# specify subplot and turn of axis
+		ax = plt.subplot(ROWS, COLUMNS, ix)
+		ax.set_xticks([])
+		ax.set_yticks([])
+		# plot filter channel in grayscale
+		plt.imshow(feature_maps_activation[0, :, :, ix-1], cmap='gray')
+		ix += 1
+# show the figure
+plt.show() 
 
